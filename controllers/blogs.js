92a9e7a5dfe230fb84eb17fc1760681f4aff21d7
Dominik Kralj <kraljdominik97@gmail.com>
2025-03-06 09:06:39 +0100

    Exercises 4.1-4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const Blog = require('../models/blog')
+const router = express.Router()
+
+router.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+router.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+module.exports = router
