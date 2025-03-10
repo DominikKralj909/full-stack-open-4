e1310e2e5f7eca1cb2e7b471c0b5b3611fc585a2
Dominik Kralj <kraljdominik97@gmail.com>
2025-03-10 15:59:49 +0100

    Exercises 4.15-4.16

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const blogsRouter = require('./controllers/blogs')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const app = express()
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
+const middleware = require('./utils/middleware');
+
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+
+const app = express();
 
 mongoose.connect(config.MONGODB_URI).then(() => logger.info('Connected to MongoDB')).catch(err => logger.error('Error connecting to MongoDB:', err))
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  name: String,
+  passwordHash: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
